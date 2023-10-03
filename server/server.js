d01b861e8f01737ee6a1892aeaa3aad29abb3149
SBJ <sbj@aol.com>
2023-10-03 12:03:39 -0400

    updated package json

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 const port = 2020;
-
+console.log(`Server is running on port ${port}`)
 app.use(express.json())
 
 app.get('/api/planets', function(req,res){
