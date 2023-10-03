5862bdd27d8bf6e8496dc56c16af733ac61ddc25
SBJ <sbj@aol.com>
2023-10-03 11:50:48 -0400

    updated server file

--- server/server.js
+++ server/server.js
@@ -2,3 +2,12 @@ import express from "express";
 
 const app = express();
 const port = 2020;
+
+app.use(express.json())
+
+app.get('/api/planets', function(req,res){
+    let test = 'fake'
+    return {test}
+} )
+
+app.listen(port);
