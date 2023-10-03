41d8c9b447e0d32816fe2be9cf28a9e9aac1504a
SBJ <sbj@aol.com>
2023-10-03 13:56:04 -0400

    added all routes to server file

--- server/server.js
+++ server/server.js
@@ -14,4 +14,64 @@ app.get('/api/planets', function(req,res){
     })
 } );
 
+app.get('/api/characters', function(req,res){
+    findAllCharacters(function(data){
+        res.send(data)
+    })
+} );
+
+app.get('/api/films', function(req,res){
+    findAllFilms(function(data){
+        res.send(data)
+    })
+} );
+
+app.get('/api/character/:id', function(req,res){
+    findCharacter(function(data){
+        res.send(data)
+    })
+} );
+
+app.get('/api/films/:id', function(req,res){
+    findFilms(function(data){
+        res.send(data)
+    })
+} );
+
+app.get('/api/planets/:id', function(req,res){
+    findPlanets(function(data){
+        res.send(data)
+    })
+} );
+
+app.get('/api/films/:id/characters', function(req,res){
+    findAllFilms(function(data){
+        res.send(data)
+    })
+} );
+
+app.get('/api/films/:id/planets', function(req,res){
+    findAllFilms(function(data){
+        res.send(data)
+    })
+} );
+
+app.get('/api/characters/:id/films', function(req,res){
+    findAllFilms(function(data){
+        res.send(data)
+    })
+} );
+
+app.get('/api/planets/:id/films', function(req,res){
+    findAllFilms(function(data){
+        res.send(data)
+    })
+} );
+
+app.get('/api/planets/:id/characters', function(req,res){
+    findAllFilms(function(data){
+        res.send(data)
+    })
+} );
+
 app.listen(port);
